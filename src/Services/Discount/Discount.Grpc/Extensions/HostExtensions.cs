cf95fb754aee80f935c4bd7270ede7583e65d413
Hamid Mohammadi <hamid.mohammadi@meditechsys.com>
2022-04-20 17:10:17 +0430

    end of Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static WebApplication MigrateDatabase<TContext>(this WebApplication host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating Postgresql Database...");
+                    using var connection = new NpgsqlConnection(configuration["DatabaseSettings:ConnectionString"]);
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(
+		                                        ID SERIAL PRIMARY KEY NOT NULL,
+		                                        ProductName     VARCHAR(24) NOT NULL,
+		                                        Description     TEXT,
+		                                        Amount          INT);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "insert into Coupon (ProductName, description, amount) values ('IPhone X', 'IPhone Discount', 150)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "insert into Coupon (ProductName, description, amount) values ('Samsung 10', 'Samsung Discount', 100)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated Postgresql Database");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the postgresql database");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
