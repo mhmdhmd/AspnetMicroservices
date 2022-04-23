21f8326692ddcd11b5b79dce71436417c5109ff9
Hamid Mohammadi <hamid.mohammadi@meditechsys.com>
2022-04-23 19:08:08 +0430

    end of command handler and validation

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(o => o.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty().WithMessage("{Email Address} is required.");
+
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty().WithMessage("{Total Price} is required.")
+                .GreaterThan(0).WithMessage("{Total Price} should be greater than zero.");
+        }
+    }
+}
